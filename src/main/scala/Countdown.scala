4bbe0a7fa7a0a5e39d8baf9baea467170041ecd2
Drew Miley <drewmiley92@gmail.com>
2018-04-12 00:18:44 +0100

    Adds some basic machinery

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,7 +2,20 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
+    def pairwiseCalculationList(a: Int, b: Int): List[Int] = {
+        List(a + b, a - b, a * b, a / b).filter(d => d > 0 && d % 1 == 0)
+    }
+
+    def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
+        listOfNumberLists
+    }
+
     def isSolvable(): Boolean = {
-        true
+        var savedValues: List[List[Int]] = List(picked)
+        var currentResult = savedValues
+        var calculatedValues = performPairwiseCalculations(currentResult)
+        savedValues = savedValues ++ calculatedValues
+        currentResult = calculatedValues
+        savedValues.filter(d => d.contains(target)).length > 0
     }
 }
