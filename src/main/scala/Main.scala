0d0e465109cfd1d854b653a9054d164e132de107
Drew Miley <drewmiley92@gmail.com>
2018-04-11 23:54:46 +0100

    Generates target number

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,13 @@ object Main extends App {
     val largePicker = new NumberPicker((1 to 4).map(d => 25 * d).toList)
     val smallPicker = new NumberPicker((1 to 10).toList ++ (1 to 10).toList)
 
+    val answerPicker = new NumberPicker((101 to 999).toList)
+
     val pickedNumbers = largePicker.select(largeNumbers) ++ smallPicker.select(smallNumbers)
 
     println(pickedNumbers)
+
+    val targetNumber = answerPicker.select(1).head
+
+    println(targetNumber)
 }
