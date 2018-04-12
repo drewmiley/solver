4079c1d098b34a31ca805a3f0ffac6a82bf0c43a
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-12 17:36:46 +0100

    WIP creating tuple + number lists

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,23 +2,33 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
-    def pairwiseCalculationList(a: Int, b: Int): List[Int] = {
-        List(a + b, a - b, a * b, a / b).filter(d => d > 0 && d % 1 == 0)
+    def pairwiseCalculationList(ints: (Int, Int)): List[Int] = {
+        List(ints._1 + ints._2, ints._2 - ints._1, ints._1 * ints._2, ints._2 / ints._1)
+                .filter(d => d > 0 && d % 1 == 0)
     }
 
     def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
+        val tupleNumberLists = listOfNumberLists
+            .map(numberList => {
+                numberList
+                    .zipWithIndex
+                    .map(d => {
+                        d._1
+                    })
+            })
+        println(tupleNumberLists)
         listOfNumberLists.map(d => d diff List(d.head))
     }
 
     def isSolvable(): Boolean = {
-        var savedValues: List[List[Int]] = List(picked)
-        var currentResult = savedValues
+        var savedValues: List[List[Int]] = List()
+        var currentResult = List(picked)
         var calculatedValues = List(List(0))
         while (currentResult.exists(d => d.size > 1)) {
             calculatedValues = performPairwiseCalculations(currentResult)
-            currentResult = calculatedValues
-            savedValues = savedValues ++ calculatedValues
+            currentResult = calculatedValues.filter(d => !d.contains(target))
+            savedValues = savedValues ++ calculatedValues.filter(d => d.contains(target))
         }
-        savedValues.exists(d => d.contains(target))
+        savedValues.size > 0
     }
 }
