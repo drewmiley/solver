ed6aa97e9f886dd8785da2708bd46db2bd11df85
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-13 12:49:56 +0100

    Renames list to set correctly

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,14 +2,14 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
-    private def pairwiseCalculationList(ints: (Int, Int)): Set[Int] = {
+    private def pairwiseCalculationSet(ints: (Int, Int)): Set[Int] = {
         Set(ints._1 + ints._2, ints._2 - ints._1, ints._1 * ints._2, ints._2 / ints._1)
             .filter(d => d > 0 && d % 1 == 0)
     }
 
-    private def getNewList(numberList: List[Int], d: (Int, Int), pairwiseList: (Set[Int], Int), pairwiseValue: Int): List[Int] = {
-        val mem = numberList diff List(numberList(d._2), numberList(d._2 + 1 + pairwiseList._2))
-        (mem :+ pairwiseValue).sorted
+    private def getNewList(fullList: List[Int], firstIndex: Int, secondIndex: Int, newValue: Int): List[Int] = {
+        val mem = fullList diff List(fullList(firstIndex), fullList(secondIndex))
+        (mem :+ newValue).sorted
     }
 
     private def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
@@ -17,8 +17,9 @@ class Countdown(picked: List[Int], target: Int) {
             numberList.zipWithIndex
                 .flatMap(d => {
                     (d._2 + 1 until numberList.size)
-                        .map(i => pairwiseCalculationList((d._1, numberList(i)))).zipWithIndex
-                        .flatMap(pairwiseList => pairwiseList._1.map(pairwiseValue => getNewList(numberList, d, pairwiseList, pairwiseValue))
+                        .map(i => (pairwiseCalculationSet((d._1, numberList(i))), i))
+                        .flatMap(pairwiseList => pairwiseList._1.map(pairwiseValue =>
+                            getNewList(numberList, d._2, pairwiseList._2, pairwiseValue))
                     )
             })
         })
