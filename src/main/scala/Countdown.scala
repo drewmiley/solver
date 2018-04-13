91e8e1f052acf37aa92f4f4ef14131092a097b68
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-13 12:16:28 +0100

    Refactors code

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,29 +2,26 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
-    def pairwiseCalculationList(ints: (Int, Int)): Set[Int] = {
+    private def pairwiseCalculationList(ints: (Int, Int)): Set[Int] = {
         Set(ints._1 + ints._2, ints._2 - ints._1, ints._1 * ints._2, ints._2 / ints._1)
-                .filter(d => d > 0 && d % 1 == 0)
+            .filter(d => d > 0 && d % 1 == 0)
     }
 
-    def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
-        listOfNumberLists
-            .map(numberList => {
-                numberList
-                    .zipWithIndex
-                    .map(d => {
-                        (d._2 + 1 until numberList.size)
-                            .map(i => pairwiseCalculationList((d._1, numberList(i))))
-                            .zipWithIndex
-                            .map(pairwiseList => {
-                                pairwiseList._1
-                                    .map(pairwiseValue => {
-                                        val mem = numberList diff List(numberList(d._2), numberList(d._2 + 1 + pairwiseList._2))
-                                        (mem :+ pairwiseValue).sorted
-                                    })
-                            })
-                    })
-            }).flatten.flatten.flatten
+    private def getNewList(numberList: List[Int], d: (Int, Int), pairwiseList: (Set[Int], Int), pairwiseValue: Int): List[Int] = {
+        val mem = numberList diff List(numberList(d._2), numberList(d._2 + 1 + pairwiseList._2))
+        (mem :+ pairwiseValue).sorted
+    }
+
+    private def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
+        listOfNumberLists.flatMap(numberList => {
+            numberList.zipWithIndex
+                .flatMap(d => {
+                    (d._2 + 1 until numberList.size)
+                        .map(i => pairwiseCalculationList((d._1, numberList(i)))).zipWithIndex
+                        .flatMap(pairwiseList => pairwiseList._1.map(pairwiseValue => getNewList(numberList, d, pairwiseList, pairwiseValue))
+                    )
+            })
+        })
     }
 
     def isSolvable(): Boolean = {
