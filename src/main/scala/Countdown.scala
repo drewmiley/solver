f6427bd99d73cae45e296208ebf5f3f9d288f3ea
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-13 11:16:23 +0100

    Countdown is solved

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,22 +2,29 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
-    def pairwiseCalculationList(ints: (Int, Int)): List[Int] = {
-        List(ints._1 + ints._2, ints._2 - ints._1, ints._1 * ints._2, ints._2 / ints._1)
+    def pairwiseCalculationList(ints: (Int, Int)): Set[Int] = {
+        Set(ints._1 + ints._2, ints._2 - ints._1, ints._1 * ints._2, ints._2 / ints._1)
                 .filter(d => d > 0 && d % 1 == 0)
     }
 
     def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
-        val tupleNumberLists = listOfNumberLists
+        listOfNumberLists
             .map(numberList => {
                 numberList
                     .zipWithIndex
                     .map(d => {
-                        d._1
+                        (d._2 + 1 until numberList.size)
+                            .map(i => pairwiseCalculationList((d._1, numberList(i))))
+                            .zipWithIndex
+                            .map(pairwiseList => {
+                                pairwiseList._1
+                                    .map(pairwiseValue => {
+                                        val mem = numberList diff List(numberList(d._2), numberList(d._2 + 1 + pairwiseList._2))
+                                        (mem :+ pairwiseValue).sorted
+                                    })
+                            })
                     })
-            })
-        println(tupleNumberLists)
-        listOfNumberLists.map(d => d diff List(d.head))
+            }).flatten.flatten.flatten
     }
 
     def isSolvable(): Boolean = {
@@ -29,6 +36,7 @@ class Countdown(picked: List[Int], target: Int) {
             currentResult = calculatedValues.filter(d => !d.contains(target))
             savedValues = savedValues ++ calculatedValues.filter(d => d.contains(target))
         }
+        println(savedValues)
         savedValues.size > 0
     }
 }
