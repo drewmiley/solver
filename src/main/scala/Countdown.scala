7ca93661fae35f9d63804f1ce5c38df02fbb8499
Drew Miley <drewmiley92@gmail.com>
2018-04-16 00:32:25 +0100

    Replaces with indexpair based flatMapping

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -14,36 +14,33 @@ class Countdown(picked: List[Int], target: Int) {
         (0 until listSize).flatMap(i => (i + 1 until listSize).map(j => (i, j))).toList
     }
 
+    def operateOnIntegerPairAndCreateNewLists(numberList: List[Int], indexPair: (Int, Int)): List[List[Int]] = {
+        val min = numberList(indexPair._1)
+        val max = numberList(indexPair._2)
+        val integerPairOperationValues = applyOperatorsToIntegerPair(min, max)
+        val listWithIndexPairRemoved = numberList diff List(min, max)
+        integerPairOperationValues.toList.map(value => (listWithIndexPairRemoved :+ value).sorted)
+    }
+
     def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
         listOfNumberLists.flatMap(numberList => {
-            // TODO: Replace with generateIndexPairs iteration
-            numberList.zipWithIndex
-                .flatMap(d => {
-                    (d._2 + 1 until numberList.size)
-                        .map(i => (applyOperatorsToIntegerPair(d._1, numberList(i)), i))
-                        .flatMap(pairwiseList => pairwiseList._1.map(pairwiseValue =>
-                            ((numberList diff List(numberList(d._2), numberList(pairwiseList._2))) :+ pairwiseValue).sorted)
-                    )
+            generateIndexPairs(numberList.size).flatMap(indexPair => {
+                operateOnIntegerPairAndCreateNewLists(numberList, indexPair)
             })
         })
     }
 
     def runSolver(picked: List[Int], target: Int): List[List[Int]] = {
         val state = new State(List(picked))
-        def recurse(state: State): State = state.currentResult.exists(d => d.size > 1) match {
-            case false => state
-            case true => {
-                val calculatedValues = performPairwiseCalculations(state.currentResult)
-                recurse(new State(calculatedValues.filter(d => !d.contains(target)),
-                    state.solutions ++ calculatedValues.filter(d => d.contains(target))))
-            }
+        def recurse(state: State): State = if (state.currentResult.exists(d => d.size > 1)) {
+            val calculatedValues = performPairwiseCalculations(state.currentResult)
+            recurse(new State(calculatedValues.filter(d => !d.contains(target)),
+                state.solutions ++ calculatedValues.filter(d => d.contains(target))))
+        } else {
+            state
         }
         recurse(state).solutions
     }
 
-    def isSolvable(): Boolean = {
-        val solutions = runSolver(picked, target)
-        println(solutions)
-        solutions.size > 0
-    }
+    def isSolvable: Boolean = runSolver(picked, target).nonEmpty
 }
