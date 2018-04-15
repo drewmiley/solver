d872956d80db51ff69364f3b0a99db4611704d07
Drew Miley <drewmiley92@gmail.com>
2018-04-15 22:21:39 +0100

    Makes private methods available

--- src/main/scala/Countdown.scala
+++ src/main/scala/Countdown.scala
@@ -2,60 +2,48 @@ package main.scala
 
 class Countdown(picked: List[Int], target: Int) {
 
-    private class State(var calculatedValues: List[List[Int]] = List(),
-                        var currentResult: List[List[Int]] = List(),
+    private class State(var currentResult: List[List[Int]] = List(),
                         var solutions: List[List[Int]] = List())
 
-    private def pairwiseCalculationSet(min: Int, max: Int): Set[Int] = {
+    def applyOperatorsToIntegerPair(min: Int, max: Int): Set[Int] = {
         (Set(min + max, max - min, min * max, max / min) diff Set(min, max))
             .filter(d => d > 0 && d % 1 == 0)
     }
 
-    private def getNewList(fullList: List[Int], firstIndex: Int, secondIndex: Int, newValue: Int): List[Int] = {
-        ((fullList diff List(fullList(firstIndex), fullList(secondIndex))) :+ newValue).sorted
-    }
-
-    private def generateIndexPairs(listSize: Int): List[(Int, Int)] = {
+    def generateIndexPairs(listSize: Int): List[(Int, Int)] = {
         (0 until listSize).flatMap(i => (i + 1 until listSize).map(j => (i, j))).toList
     }
 
-    private def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
+    def performPairwiseCalculations(listOfNumberLists: List[List[Int]]): List[List[Int]] = {
         listOfNumberLists.flatMap(numberList => {
             // TODO: Replace with generateIndexPairs iteration
             numberList.zipWithIndex
                 .flatMap(d => {
                     (d._2 + 1 until numberList.size)
-                        .map(i => (pairwiseCalculationSet(d._1, numberList(i)), i))
+                        .map(i => (applyOperatorsToIntegerPair(d._1, numberList(i)), i))
                         .flatMap(pairwiseList => pairwiseList._1.map(pairwiseValue =>
-                            getNewList(numberList, d._2, pairwiseList._2, pairwiseValue))
+                            ((numberList diff List(numberList(d._2), numberList(pairwiseList._2))) :+ pairwiseValue).sorted)
                     )
             })
         })
     }
 
-    private def iterate(state: State): State = {
-        var solutions = state.solutions
-        var currentResult = state.currentResult
-        var calculatedValues = state.calculatedValues
-        calculatedValues = performPairwiseCalculations(currentResult)
-        currentResult = calculatedValues.filter(d => !d.contains(target))
-        solutions = solutions ++ calculatedValues.filter(d => d.contains(target))
-        new State(calculatedValues, currentResult, solutions)
-    }
-
-    private def runSolver(initialState: State): State = {
-        var state = initialState
+    def runSolver(picked: List[Int], target: Int): List[List[Int]] = {
+        val state = new State(List(picked))
         def recurse(state: State): State = state.currentResult.exists(d => d.size > 1) match {
             case false => state
-            case true => recurse(iterate(state))
+            case true => {
+                val calculatedValues = performPairwiseCalculations(state.currentResult)
+                recurse(new State(calculatedValues.filter(d => !d.contains(target)),
+                    state.solutions ++ calculatedValues.filter(d => d.contains(target))))
+            }
         }
-        recurse(state)
+        recurse(state).solutions
     }
 
     def isSolvable(): Boolean = {
-        var initialState = new State(List(), List(picked), List())
-        val state = runSolver(initialState)
-        println(state.solutions)
-        state.solutions.size > 0
+        val solutions = runSolver(picked, target)
+        println(solutions)
+        solutions.size > 0
     }
 }
