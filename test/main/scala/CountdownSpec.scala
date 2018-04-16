a6200455ac13e4cb6868871a97840307eae7f875
Drew Miley <drewmiley92@gmail.com>
2018-04-16 11:27:41 +0100

    Adds some property testing

--- test/main/scala/CountdownSpec.scala
+++ test/main/scala/CountdownSpec.scala
@@ -10,19 +10,55 @@ class CountdownSpec extends FlatSpec with BeforeAndAfterEach {
     countdown = new Countdown(List(), 0)
   }
 
-  "applyOperatorsToIntegerPair" should "be a set of size 1 for param 1, 1" in {
-    assert(countdown.applyOperatorsToIntegerPair(1, 1).size == 1)
+  "applyOperatorsToIntegerPair" should "be Set(2) for param 1, 1" in {
+    assert(countdown.applyOperatorsToIntegerPair(1, 1) == Set(2))
   }
 
-  "generateIndexPairs" should "be a list of size 1 for param 2" in {
-    assert(countdown.generateIndexPairs(2).size == 1)
+  "applyOperatorsToIntegerPair" should "be a set of at most size 3 for param i, i" in {
+    (2 to 100).foreach(i => assert(countdown.applyOperatorsToIntegerPair(i, i).size <= 3))
   }
 
-  "operateOnIntegerPairAndCreateNewLists" should "be a list of size 1 for params List(1, 1), (0, 1)" in {
-    assert(countdown.operateOnIntegerPairAndCreateNewLists(List(1, 1), (0, 1)).size == 1)
+  "applyOperatorsToIntegerPair" should "be a set of at most size 4 for param i, j" in {
+    (2 to 100).foreach(i => (i + 1  to 100).foreach(j => assert(countdown.applyOperatorsToIntegerPair(i, j).size <= 4)))
   }
 
-  "performOneOperationOnCurrentLists" should "be a list of size 1 for param List(List(1, 1))" in {
-    assert(countdown.performOneOperationOnCurrentLists(List(List(1, 1))).size == 1)
+  "applyOperatorsToIntegerPair" should "not return any value <= 0 for param i, j" in {
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(countdown.applyOperatorsToIntegerPair(i, j).forall(value => value > 0))))
+  }
+
+  "applyOperatorsToIntegerPair" should "not return any non-int value for param i, j" in {
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(countdown.applyOperatorsToIntegerPair(i, j).forall(value => value % 1 == 0))))
+  }
+
+  "applyOperatorsToIntegerPair" should "not return i or j for param i, j" in {
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(countdown.applyOperatorsToIntegerPair(i, j).forall(value => !List(i, j).contains(value)))))
+  }
+
+  "generateIndexPairs" should "be List((0, 1)) for param 2" in {
+    assert(countdown.generateIndexPairs(2) == List((0, 1)))
+  }
+
+  "generateIndexPairs" should "be a list of size T(n - 1) for param n >= 2" in {
+    (2 to 100).foreach(n => assert(countdown.generateIndexPairs(n).size == n * (n - 1) / 2))
+  }
+
+  "generateIndexPairs" should "be a list of tuples with _2 > _1" in {
+    (2 to 100).foreach(n => assert(countdown.generateIndexPairs(n).forall(indexPair => indexPair._1 < indexPair._2)))
+  }
+
+  "generateIndexPairs" should "be a list of tuples with 1 <= _2 <= n" in {
+    (2 to 100).foreach(n => assert(countdown.generateIndexPairs(n).forall(indexPair => (1 to n).contains(indexPair._2))))
+  }
+
+  "generateIndexPairs" should "be a list of tuples with 0 <= _2 <= n + 1" in {
+    (2 to 100).foreach(n => assert(countdown.generateIndexPairs(n).forall(indexPair => (0 to n + 1).contains(indexPair._1))))
+  }
+
+  "operateOnIntegerPairAndCreateNewLists" should "be a List(List(2)) for params List(1, 1), (0, 1)" in {
+    assert(countdown.operateOnIntegerPairAndCreateNewLists(List(1, 1), (0, 1)) == List(List(2)))
+  }
+
+  "performOneOperationOnCurrentLists" should "be a List(List(2)) for param List(List(1, 1))" in {
+    assert(countdown.performOneOperationOnCurrentLists(List(List(1, 1))) == List(List(2)))
   }
 }
