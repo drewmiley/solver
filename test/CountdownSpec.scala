e415c97734b5a7151979219a363e5f1d12c6f05c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-05-22 19:09:00 +0100

    Implements tests for getNewState

--- test/CountdownSpec.scala
+++ test/CountdownSpec.scala
@@ -1,5 +1,6 @@
 package main
 
+import Countdown._
 import org.scalatest._
 
 class CountdownSpec extends FlatSpec with BeforeAndAfterEach {
@@ -9,75 +10,75 @@ class CountdownSpec extends FlatSpec with BeforeAndAfterEach {
   }
 
   "applyOperatorsToIntegerPair" should "be List(Operation(2,1 + 1 = 2)) for param 1, 1" in {
-    assert(Countdown.applyOperatorsToIntegerPair(1, 1) == List(Operation(2,"1 + 1 = 2")))
+    assert(applyOperatorsToIntegerPair(1, 1) == List(Operation(2,"1 + 1 = 2")))
   }
 
   "applyOperatorsToIntegerPair" should "be a set of at most size 3 for param i, i" in {
-    (2 to 100).foreach(i => assert(Countdown.applyOperatorsToIntegerPair(i, i).size <= 3))
+    (2 to 100).foreach(i => assert(applyOperatorsToIntegerPair(i, i).size <= 3))
   }
 
   "applyOperatorsToIntegerPair" should "be a set of at most size 4 for param i, j" in {
-    (2 to 100).foreach(i => (i + 1  to 100).foreach(j => assert(Countdown.applyOperatorsToIntegerPair(i, j).size <= 4)))
+    (2 to 100).foreach(i => (i + 1  to 100).foreach(j => assert(applyOperatorsToIntegerPair(i, j).size <= 4)))
   }
 
   "applyOperatorsToIntegerPair" should "not return any value <= 0 for param i, j" in {
-    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(Countdown.applyOperatorsToIntegerPair(i, j).forall(_.value > 0))))
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(applyOperatorsToIntegerPair(i, j).forall(_.value > 0))))
   }
 
   "applyOperatorsToIntegerPair" should "not return any non-int value for param i, j" in {
-    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(Countdown.applyOperatorsToIntegerPair(i, j).forall(_.value % 1 == 0))))
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(applyOperatorsToIntegerPair(i, j).forall(_.value % 1 == 0))))
   }
 
   "applyOperatorsToIntegerPair" should "not return i or j for param i, j" in {
-    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(Countdown.applyOperatorsToIntegerPair(i, j).forall(operation => !List(i, j).contains(operation.value)))))
+    (2 to 100).foreach(i => (i + 1 to 100).foreach(j => assert(applyOperatorsToIntegerPair(i, j).forall(operation => !List(i, j).contains(operation.value)))))
   }
 
   "generateIndexPairs" should "be List((0, 1)) for param 2" in {
-    assert(Countdown.generateIndexPairs(2) == List((0, 1)))
+    assert(generateIndexPairs(2) == List((0, 1)))
   }
 
   "generateIndexPairs" should "be a list of size T(n - 1) for param n >= 2" in {
-    (2 to 100).foreach(n => assert(Countdown.generateIndexPairs(n).size == n * (n - 1) / 2))
+    (2 to 100).foreach(n => assert(generateIndexPairs(n).size == n * (n - 1) / 2))
   }
 
   "generateIndexPairs" should "be a list of tuples with _2 > _1" in {
-    (2 to 100).foreach(n => assert(Countdown.generateIndexPairs(n).forall(indexPair => indexPair._1 < indexPair._2)))
+    (2 to 100).foreach(n => assert(generateIndexPairs(n).forall(indexPair => indexPair._1 < indexPair._2)))
   }
 
   "generateIndexPairs" should "be a list of tuples with 1 <= _2 <= n" in {
-    (2 to 100).foreach(n => assert(Countdown.generateIndexPairs(n).forall(indexPair => (1 to n).contains(indexPair._2))))
+    (2 to 100).foreach(n => assert(generateIndexPairs(n).forall(indexPair => (1 to n).contains(indexPair._2))))
   }
 
   "generateIndexPairs" should "be a list of tuples with 0 <= _2 <= n + 1" in {
-    (2 to 100).foreach(n => assert(Countdown.generateIndexPairs(n).forall(indexPair => (0 to n + 1).contains(indexPair._1))))
+    (2 to 100).foreach(n => assert(generateIndexPairs(n).forall(indexPair => (0 to n + 1).contains(indexPair._1))))
   }
 
   "operateOnIntegerPairAndCreateNewLists" should "be a List(Calculation(List(2),List(1 + 1 = 2))) for params List(1, 1), (0, 1)" in {
-    assert(Countdown.operateOnIntegerPairAndCreateNewLists(Calculation(List(1, 1)), (0, 1)) == List(Calculation(List(2),List("1 + 1 = 2"))))
+    assert(operateOnIntegerPairAndCreateNewLists(Calculation(List(1, 1)), (0, 1)) == List(Calculation(List(2),List("1 + 1 = 2"))))
   }
 
   "operateOnIntegerPairAndCreateNewLists" should "be a List of at most size 4" in {
     (2 to 6).foreach(size =>
       (1 to 100).foreach(_ =>
-        assert(Countdown.operateOnIntegerPairAndCreateNewLists(Calculation(generateSortedIntList(size)), (0, 1)).size <= 4)))
+        assert(operateOnIntegerPairAndCreateNewLists(Calculation(generateSortedIntList(size)), (0, 1)).size <= 4)))
   }
 
   "operateOnIntegerPairAndCreateNewLists" should "be a List of lists with size 1 less than the param list" in {
     (2 to 6).foreach(size =>
       (1 to 100).foreach(_ =>
-        assert(Countdown.operateOnIntegerPairAndCreateNewLists(Calculation(generateSortedIntList(size)), (0, 1))
+        assert(operateOnIntegerPairAndCreateNewLists(Calculation(generateSortedIntList(size)), (0, 1))
           .map(list => list.values.size).toSet == Set(size - 1))))
   }
 
   "performOneOperationOnCurrentLists" should "be a List(Calculation(List(2),List(1 + 1 = 2))) for param List(List(1, 1))" in {
-    assert(Countdown.performOneOperationOnCurrentLists(List(Calculation(List(1, 1)))) == List(Calculation(List(2),List("1 + 1 = 2"))))
+    assert(performOneOperationOnCurrentLists(List(Calculation(List(1, 1)))) == List(Calculation(List(2),List("1 + 1 = 2"))))
   }
 
   "performOneOperationOnCurrentLists" should "be a List of at most 4 * T(size - 1) times the size of the param lists" in {
     (2 to 6).foreach(size =>
       (1 to 100).foreach(_ => {
         val generatedList = (1 to 100).toList.map(_ => generateSortedIntList(size))
-        assert(Countdown.performOneOperationOnCurrentLists(generatedList.map(Calculation(_))).size <= 4 * generatedList.size * (size * (size - 1) / 2))
+        assert(performOneOperationOnCurrentLists(generatedList.map(Calculation(_))).size <= 4 * generatedList.size * (size * (size - 1) / 2))
       }))
   }
 
@@ -85,7 +86,7 @@ class CountdownSpec extends FlatSpec with BeforeAndAfterEach {
     (2 to 6).foreach(size =>
       (1 to 100).foreach(_ => {
         val generatedList = (1 to 100).toList.map(_ => generateSortedIntList(size))
-        assert(Countdown.performOneOperationOnCurrentLists(generatedList.map(Calculation(_)))
+        assert(performOneOperationOnCurrentLists(generatedList.map(Calculation(_)))
           .map(list => list.values.size).toSet == Set(size - 1))
       }))
   }
@@ -94,33 +95,54 @@ class CountdownSpec extends FlatSpec with BeforeAndAfterEach {
     (2 to 6).foreach(size =>
       (1 to 100).foreach(_ => {
         val generatedList = (1 to 100).toList.map(_ => generateSortedIntList(size))
-        val targetNumber = new NumberPicker((1 to 100).toList).select(1).head
+        val target = new NumberPicker((1 to 100).toList).select(1).head
         generatedList.foreach(picked => {
           val state: State = State(List(Calculation(picked)))
-          val newState = Countdown.getNewState(targetNumber, filterDuplicate = false)(state)
+          val newState = getNewState(target, filterDuplicate = false)(state)
           val newCalculations = newState.currentResult
-          assert(Countdown.filterDuplicateCalculations(newCalculations).length == newCalculations.groupBy(_.values).values.flatten.toList.length)
+          assert(filterDuplicateCalculations(newCalculations).length == newCalculations.groupBy(_.values).values.flatten.toList.length)
         })
       }))
   }
 
   "getNewState" should "return a State where for values in the list, the size is 1 less than for the State passed in" in {
     (2 to 6).foreach(size =>
-      (1 to 100).foreach(_ => {
+      (1 to 2).foreach(_ => {
         val generatedList = (1 to 100).toList.map(_ => generateSortedIntList(size))
-//        TODO: Write correct assertion
-//        assert(Countdown.performOneOperationOnCurrentLists(generatedList.map(Calculation(_)))
-//          .map(list => list.values.size).toSet == Set(size - 1))
+        val target = new NumberPicker((1 to 100).toList).select(1).head
+        val initGetNewState: State => State = getNewState(target, filterDuplicate = true)
+        generatedList.foreach(picked => {
+          val initState: State = State(List(Calculation(picked)))
+          var state: State = initState
+          (1 until size).foreach(stepNo => {
+            val newState: State = initGetNewState(state)
+            val stateValuesLength = state.currentResult.map(_.values.length).toSet
+            val newStateValuesLength = newState.currentResult.map(_.values.length).toSet
+            assert(stateValuesLength.size == 1)
+            assert(newStateValuesLength.size == 1)
+            assert(stateValuesLength.head == size + 1 - stepNo)
+            assert(newStateValuesLength.head + 1 == size + 1 - stepNo)
+            state = newState
+          })
+        })
       }))
   }
 
   "getNewState" should "return a State where the size of solutions is greater than or equal to for the State passed in" in {
     (2 to 6).foreach(size =>
-      (1 to 100).foreach(_ => {
+      (1 to 2).foreach(_ => {
         val generatedList = (1 to 100).toList.map(_ => generateSortedIntList(size))
-//        TODO: Write correct assertion
-//        assert(Countdown.performOneOperationOnCurrentLists(generatedList.map(Calculation(_)))
-//          .map(list => list.values.size).toSet == Set(size - 1))
+        val target = new NumberPicker((1 to 100).toList).select(1).head
+        val initGetNewState: State => State = getNewState(target, filterDuplicate = true)
+        generatedList.foreach(picked => {
+          val initState: State = State(List(Calculation(picked)))
+          var state: State = initState
+          (1 until size).foreach(stepNo => {
+            val newState: State = initGetNewState(state)
+            assert(newState.solutions.length >= state.solutions.length)
+            state = newState
+          })
+        })
       }))
   }
 }
