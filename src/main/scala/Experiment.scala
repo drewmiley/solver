758835bb8b1d6b4df45e34551c9403f0c8fd6f9c
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-06-29 21:22:02 +0100

    Removes some no longer needed lines from experiment routine

--- src/main/scala/Experiment.scala
+++ src/main/scala/Experiment.scala
@@ -8,13 +8,10 @@ object Experiment {
   def runExperiment(): Unit = {
     val pickedNumbers = getPickedNumbers(Some(List(1, 2, 3, 4, 6, 20)))
 
-    val targetMin: Option[Int] = Some(100)
-    val targetMax: Option[Int] = Some(999)
-
     printValue("Picked", pickedNumbers.mkString(", "))
 
-    val targetRangeLower = targetMin.getOrElse(101)
-    val targetRangeHigher = targetMax.getOrElse(999)
+    val targetRangeLower = 101
+    val targetRangeHigher = 999
     val targetRange = targetRangeLower to targetRangeHigher
     printValue("Target Min", targetRangeLower.toString)
     printValue("Target Max", targetRangeHigher.toString)
