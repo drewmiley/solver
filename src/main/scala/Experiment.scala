ee48432316e1ad0f90ec9713750950951befc8cb
Drew Miley <13115186+drewmiley@users.noreply.github.com>
2026-06-29 22:35:02 +0100

    Adds large/small permutations values

--- src/main/scala/Experiment.scala
+++ src/main/scala/Experiment.scala
@@ -5,6 +5,21 @@ import main.Util.{getPickedNumbers, printValue}
 
 object Experiment {
 
+  private val largeNumbers: List[Int] = (1 to 4).map(d => 25 * d).toList
+  private val smallNumbers: List[Int] = (1 to 10).toList ++ (1 to 10).toList
+
+  private val all0LargePermuations: List[List[Int]] = List.empty
+  private val all1LargePermuations: List[List[Int]] = largeNumbers.combinations(1).toList
+  private val all2LargePermuations: List[List[Int]] = largeNumbers.combinations(2).toList
+  private val all3LargePermuations: List[List[Int]] = largeNumbers.combinations(3).toList
+  private val all4LargePermuations: List[List[Int]] = largeNumbers.combinations(4).toList
+
+  private val all6SmallPermuations: List[List[Int]] = smallNumbers.combinations(6).toList
+  private val all5SmallPermuations: List[List[Int]] = smallNumbers.combinations(5).toList
+  private val all4SmallPermuations: List[List[Int]] = smallNumbers.combinations(4).toList
+  private val all3SmallPermuations: List[List[Int]] = smallNumbers.combinations(3).toList
+  private val all2SmallPermuations: List[List[Int]] = smallNumbers.combinations(2).toList
+
   def runExperiment(): Unit = {
     val pickedNumbers = getPickedNumbers(Some(List(1, 2, 3, 4, 6, 20)))
 
@@ -18,6 +33,13 @@ object Experiment {
 
     val numbersLeftToSolve: List[Int] = solveForNoSolutions(pickedNumbers, targetRange).numbersLeftToSolve
     printValue("No Solutions for", numbersLeftToSolve.mkString(", "))
+
+    //  TODO: Implement
+    val all0Large6SmallCombinations: List[List[Int]] = List.empty
+    val all1Large5SmallCombinations: List[List[Int]] = List.empty
+    val all2Large4SmallCombinations: List[List[Int]] = List.empty
+    val all3Large3SmallCombinations: List[List[Int]] = List.empty
+    val all4Large2SmallCombinations: List[List[Int]] = List.empty
   }
 
 }
